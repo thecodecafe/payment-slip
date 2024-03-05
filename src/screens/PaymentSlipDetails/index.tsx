2f553cdb15519360dc628272492fd4f5d89a39d1
Daniel Barde <mrbarde@gmail.com>
2024-03-05 12:38:41 +0100

    style(paymentslipdetails): stick to 2 spaces for indentations

--- src/screens/PaymentSlipDetails/index.tsx
+++ src/screens/PaymentSlipDetails/index.tsx
@@ -1,5 +1,5 @@
 import {IPaymentSlipDetails} from "./interfaces";
 
 export const PaymentSlipsDetails: IPaymentSlipDetails = function PaymentSlipsDetails() {
-    return (<>Payment Slip</>);
+  return (<>Payment Slip</>);
 }
