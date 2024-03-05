c0cfd2738dc97c8424dcb1ae698c5c7e632402b6
Daniel Barde <mrbarde@gmail.com>
2024-03-05 13:49:23 +0100

    feat(data): add dummy payment slips

--- src/configs/data.ts
+++ src/configs/data.ts
@@ -5,4 +5,77 @@ export type IPaymentSlip = {
   file: string;
 }
 
-export const SLIPS: IPaymentSlip[] = [];
+export const SLIPS: IPaymentSlip[] = [
+  {
+    id: '983983h4h34h3984h938h4983g49g434938489',
+    fromDate: '2023-01-01T00:00:00',
+    toDate: '2023-01-25T23:59:59',
+    file: 'https://file.com/pdf',
+  },
+  {
+    id: '839h9834983h985h8934h938h5938h4983h4983',
+    fromDate: '2023-02-01T00:00:00',
+    toDate: '2023-02-25T23:59:59',
+    file: 'https://file.com/pdf',
+  },
+  {
+    id: '98349y384938h498359834h98359849384h3894h',
+    fromDate: '2023-03-01T00:00:00',
+    toDate: '2023-03-25T23:59:59',
+    file: 'https://file.com/pdf',
+  },
+  {
+    id: '98y34983y498y3984y9384y983y4983y498y',
+    fromDate: '2023-04-01T00:00:00',
+    toDate: '2023-04-25T23:59:59',
+    file: 'https://file.com/pdf',
+  },
+  {
+    id: '783534893943984gt384t938g49833984h983',
+    fromDate: '2023-05-01T00:00:00',
+    toDate: '2023-05-25T23:59:59',
+    file: 'https://file.com/pdf',
+  },
+  {
+    id: '983598h359839843985g983g98g2984298g',
+    fromDate: '2023-06-01T00:00:00',
+    toDate: '2023-06-25T23:59:59',
+    file: 'https://file.com/pdf',
+  },
+  {
+    id: '209u02048y3983984y9380834y934u983y4',
+    fromDate: '2023-07-01T00:00:00',
+    toDate: '2023-07-25T23:59:59',
+    file: 'https://file.com/pdf',
+  },
+  {
+    id: '38h093h498h3948h35984980298h498h3498h',
+    fromDate: '2023-08-01T00:00:00',
+    toDate: '2023-08-25T23:59:59',
+    file: 'https://file.com/pdf',
+  },
+  {
+    id: '28h928h98h29838g4983498h3948h938983h49h',
+    fromDate: '2023-09-01T00:00:00',
+    toDate: '2023-09-25T23:59:59',
+    file: 'https://file.com/pdf',
+  },
+  {
+    id: '9834h983h498389g3984983g4983g498g3498g34',
+    fromDate: '2023-10-01T00:00:00',
+    toDate: '2023-10-25T23:59:59',
+    file: 'https://file.com/pdf',
+  },
+  {
+    id: '398h493h983h498h5398h49824h02h49882h4982h3',
+    fromDate: '2023-11-01T00:00:00',
+    toDate: '2023-11-25T23:59:59',
+    file: 'https://file.com/pdf',
+  },
+  {
+    id: '893493y4084y93y04983y983498g3984g394g938983',
+    fromDate: '2023-12-01T00:00:00',
+    toDate: '2023-12-25T23:59:59',
+    file: 'https://file.com/pdf',
+  }
+];
