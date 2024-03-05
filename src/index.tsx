3ce0b7895d3bf3dc1ce8822022a29b4d48913809
Daniel Barde <mrbarde@gmail.com>
2024-03-05 12:03:48 +0100

    feat(index): setup routes

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {PaymentSlips} from './screens/PaymentSlips';
+import {PaymentSlipsDetails} from './screens/PaymentSlipDetails';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<PaymentSlips />} />
+          <Route path="/slips/:id" element={<PaymentSlipsDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
