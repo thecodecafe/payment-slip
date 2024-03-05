b1d15b24915a14130deca995972cd5937a284387
Daniel Barde <mrbarde@gmail.com>
2024-03-05 12:57:23 +0100

    refactor(findpaymentslip.api): change return type to data

--- src/apis/FindPaymentSlip.api.ts
+++ src/apis/FindPaymentSlip.api.ts
@@ -1,18 +1,16 @@
 import {IPaymentSlip, SLIPS} from "../configs/data";
 import {Sleep} from "../utils/Sleep.util";
 import {ResourceNotFoundError} from "../errors/ResourceNotFoundError";
-import {IApiResult} from "./interfaces";
 
-export async function FindPaymentSlipApi(id: string): Promise<IApiResult<IPaymentSlip>> {
+export async function FindPaymentSlipApi(id: string): Promise<IPaymentSlip> {
  try {
    await Sleep(Math.round(Math.random() * 1999));
    const slip = SLIPS.find(i => i.id === id);
    if (!slip) {
     throw new ResourceNotFoundError('Payment slip not found.');
    }
-  return Promise.resolve({data: slip});
+  return Promise.resolve(slip);
  } catch (e) {
-    const errorMessage = (e as Error).message;
-    return Promise.resolve({errorMessage});
+  return Promise.reject(e)
  }
 }
