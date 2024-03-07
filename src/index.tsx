31b69b5ad101475c4921e059e0580a7c057f43ec
Daniel Barde <mrbarde@gmail.com>
2024-03-07 13:14:05 +0100

    fix(index): add missing components

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
+import {App} from './App';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
