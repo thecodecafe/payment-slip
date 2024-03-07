e0cc9ea9a030830264aabf013f15b4125a2cc17e
Daniel Barde <mrbarde@gmail.com>
2024-03-07 13:14:20 +0100

    feat(index): initialize ionic styles for react

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
+import './ionicstyles'
+import './theme/variables.scss'
+import {setupIonicReact} from '@ionic/react';
 import {App} from './App';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {BrowserRouter} from 'react-router-dom';
+setupIonicReact()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
