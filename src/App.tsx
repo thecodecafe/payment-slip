62291a60c95678331c04d2d580a884ab124d817e
Daniel Barde <mrbarde@gmail.com>
2024-03-09 20:21:57 +0100

    refactor: rename page to page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import {Route, Routes} from "react-router-dom";
-import {Page} from "./components/Page";
+import {PageLayout} from "./components/PageLayout";
 import {PaymentSlipsDetails} from "./screens/PaymentSlipDetails";
 import {PaymentSlips} from "./screens/PaymentSlips";
 
 export function App() {
   return (
     <Routes>
-      <Route path="/" element={<Page />}>
+      <Route path="/" element={<PageLayout />}>
         <Route index element={<PaymentSlips />} />
         <Route path="/slips/:id" element={<PaymentSlipsDetails />} />
       </Route>
--- src/components/Page/interfaces.ts
+++
@@ -1,3 +0,0 @@
-export type IPage = React.FC<{
-  noHeader?: boolean;
-}>;
